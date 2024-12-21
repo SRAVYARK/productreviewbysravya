37e659c44c7d0353ed098049b93b6d793d804744
SRAVYARK <sravya2721@gmail.com>
2024-12-21 13:41:49 +0530

    PRODUCT-REVIEW submitted

--- product-review/src/App.jsx
+++ product-review/src/App.jsx
@@ -1,16 +1,12 @@
-// src/App.jsx
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import { ThemeProvider } from './contexts/ThemeContext';
 import ProductList from './components/ProductList';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider>
-        <ProductList />
-      </ThemeProvider>
+      <ProductList />
     </Provider>
   );
 };
